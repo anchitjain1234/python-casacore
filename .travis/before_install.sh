82d389c2dc3944492511c88e96312c76d76e5772
Gijs Molenaar <gijs@pythonic.nl>
2016-03-01 13:07:10 +0200

    try generic image

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -8,10 +8,14 @@ if [ "$TRAVIS_OS_NAME" = osx ]; then
     brew tap homebrew/science
     curl -L -O https://bintray.com/artifact/download/casacore/homebrew-bottles/casacore-2.0.3.el_capitan.bottle.tar.gz
     brew install ./casacore-2.0.3.el_capitan.bottle.tar.gz
+    brew install python python3 
 else
    sudo apt-get update -qq
    sudo apt-get install software-properties-common python-setuptools libboost-python-dev libcfitsio3-dev
    sudo add-apt-repository -y ppa:radio-astro/main
    sudo apt-get update -qq
-   sudo apt-get install libcasacore2-dev casacore-data
+   sudo apt-get install libcasacore2-dev casacore-data python-pip python3
 fi
+
+pip install --upgrade pip
+pip install tox

f68c6fbbabb9ae7fe0a800511d6b7b981d6f1a26
Anchit Jain <anchitjain1234@gmail.com>
2016-03-11 18:24:47 +0530

    Added gaussian1d tests

--- tests/test_funcionals.py
+++ tests/test_funcionals.py
@@ -7,3 +7,25 @@ class TestFunctionals(unittest.TestCase):
         #self.functional = functionals.functional(name='test')
         #functionals.chebyshev(self.functional)
         return
+
+    def test_gaussian1d(self):
+        gauss1d = functionals.gaussian1d([1, 2, 3])
+
+        assert gauss1d.ndim() == 1
+        assert gauss1d.npar() == 3
+        assert gauss1d.__len__() == 3
+
+        assert gauss1d.get_parameters() == [1, 2, 3]
+        gauss1d.set_parameters([3, 4, 5])
+        assert gauss1d.get_parameters() == [3, 4, 5]
+        gauss1d.set_parameter(0, 2)
+        assert gauss1d.get_parameters()[0] == 2
+
+        assert gauss1d.get_masks() == [True, True, True]
+        gauss1d.set_mask(0, False)
+        assert gauss1d.get_masks() == [False, True, True]
+        # gauss1d.set_masks([True, True, True])
+        # assert gauss1d.get_masks() == [True, True, True]
+
+        gauss1d.set_parameters([0, 0, 1])
+        assert gauss1d.f(0) == [0]
